8ae65eaebdf30612e5e03eb332d4c98194850bf1
Vir Codificat <vircodificat@gmail.com>
2025-06-19 17:54:45 -0700

    rust: Cargo installs.

---
+++ installers/cargo_installs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cargo install cargo-binstall --version 1.13.0
+
+cargo binstall fd-find      -y --version 10.2.0
+cargo binstall ripgrep      -y --version 14.1.1
+cargo binstall bat          -y --version 0.25.0
+cargo binstall exa          -y --version 0.10.1
+cargo binstall du-dust      -y --version 1.2.1
+cargo binstall tokei        -y --version 12.1.2
+cargo binstall hexyl        -y --version 0.16.0
