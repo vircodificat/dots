624ef7c2c6821d619cb8674be9b0da59245bb689
Vir Codificat <vircodificat@gmail.com>
2025-08-21 19:31:57 -0700

    bash: Fix up aliases so they aren't set if the target executable doesn't exist.

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,8 +1,4 @@
 alias reload='source $HOME/.bashrc'
-alias vi='nvim'
-alias vim='nvim'
-alias ls='exa --color=auto'
-alias df='dfrs'
 
 function path() {
     for p in $(echo $PATH | sed 's/:/ /g')
@@ -14,3 +10,27 @@ function path() {
 function exes() {
     IFS=:; for dir in $PATH; do find "$dir" -maxdepth 1 -type f -executable 2>/dev/null; done
 }
+
+function executable() {
+    exe=$1
+    command -v $exe >/dev/null 2>&1
+    return $?
+}
+
+if executable "uv"; then
+    alias python="uv run python"
+    alias pip="uv run pip"
+fi
+
+if executable "dfrs"; then
+    alias df="dfrs"
+fi
+
+if executable "nvim"; then
+    alias vi='nvim'
+    alias vim='nvim'
+fi
+
+if executable "exa"; then
+    alias ls='exa --color=auto'
+fi
