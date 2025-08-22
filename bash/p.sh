79d4eb381a61ddb61bddb4571e3088e5441375bd
Vir Codificat <vircodificat@gmail.com>
2025-08-22 09:14:53 -0700

    bash: Add -r and -l flags to p.

--- bash/p.sh
+++ bash/p.sh
@@ -1,24 +1,41 @@
 function p() {
-    if [ -z "$1" ]
-    then
+    if [ -z "$1" ]; then
         cd "$HOME/projects"
+    elif [[ "$1" == "-l" ]]; then
+        ln -s "$HOME/repos/$2" "$HOME/projects/" && \
+            echo "$HOME/projects/$2"
+    elif [[ "$1" == "-l" ]]; then
+        ln -s "$HOME/repos/$2" "$HOME/projects/" && \
+            echo "$HOME/projects/$2"
+    elif [[ "$1" == "-r" ]]; then
+        # delete only if a symlink
+        find "$HOME/projects/$2" -type l -delete && \
+            echo "Removed $HOME/projects/$2"
+    elif [ -n "$TMUX" ]; then
+        cd "$HOME/projects/$1" && \
+            tmux rename-window "$1"
     else
-        if [ -n "$TMUX" ]
-        then
-            cd "$HOME/projects/$1" && \
-                tmux rename-window "$1"
-        else
-            cd "$HOME/projects/$1"
-        fi
+        cd "$HOME/projects/$1"
     fi
 }
 
 _p_completion()
 {
     CURRENT_WORD=${COMP_WORDS[$COMP_CWORD]}
-    projects=$(/bin/ls "$HOME/projects")
-    projects_array=($projects)
-    COMPREPLY=( $(compgen -W "$projects" "$CURRENT_WORD") )
+
+    flag=${COMP_WORDS[1]}
+
+    if [[ "$flag" == "-l" ]]; then
+        # use of /bin/ls because of possible remapping of ls
+        repos=$(/bin/ls "$HOME/repos")
+        COMPREPLY=( $(compgen -W "$repos" -- "$CURRENT_WORD") )
+    elif [[ "$flag" == "-r" ]]; then
+        projects=$(find "$HOME/projects" -type l -printf "%f ")
+        COMPREPLY=( $(compgen -W "$projects" -- "$CURRENT_WORD") )
+    else
+        projects=$(/bin/ls "$HOME/projects")
+        COMPREPLY=( $(compgen -W "$projects" -- "$CURRENT_WORD") )
+    fi
 }
 
 complete -F _p_completion p
